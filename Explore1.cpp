a427f8ffea28d6be6e01173e77c5756d4e153df0
NikitaSAVIN1 <>
2022-02-02 17:18:50 +0000

    Explore1 working

--- Explore1.cpp
+++ Explore1.cpp
@@ -15,8 +15,13 @@ int main() {
     b.x = 4.5;
     b.y = 7.8;
 
+    Point c;
+    c.x = 8;
+    c.y = 7.2;
+
     cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
     cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
+    cout << "Point c: x = " << c.x << ", y = " << c.y << endl;
 
     // Add your changes here.
 
