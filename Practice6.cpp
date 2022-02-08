0ce9d020fb6986982ad4410383ea3d23634ee893
NikitaSAVIN1 <>
2022-02-08 17:12:55 +0000

    Person6 working

---
+++ Practice6.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// Put the person struct here
+
+struct Person {
+    string firstname;
+    string secondname;
+    Person();
+    Person(string f, string l);
+};
+
+Person::Person() {
+  firstname = "Unknown";
+  secondname = "Unknown";
+}
+
+Person::Person(string f, string l) {
+  firstname = f;
+  secondname = l;
+}
+
+void PrintPerson(Person x) {
+  cout << "Person:" << x.firstname << " " << x.secondname << endl;
+}
+
+void ChangeLastName(Person &n, string Lname) {
+  n.secondname = Lname;
+}
+
+int main() {
+	// Create two instances of person here, and assign the names.
+
+  Person a;
+  PrintPerson(a);
+  a.firstname = "Dorian";
+  a.secondname = "Grey";
+  PrintPerson(a);
+  
+
+  Person b("linus", "Van Pelt");
+  PrintPerson(b);
+  b.firstname = "Charlie";
+  b.secondname = "Brown";
+  PrintPerson(b);
+  ChangeLastName(b, "Torvalds");
+  PrintPerson(b);
+
+
+	return 0;
+}
